5622169c6bfb339f15410211e9b584fc46b85a1d
Martin Algesten <martin@algesten.se>
2013-11-11 15:36:42 +0530

    missed params in resource

--- resources/dfile.rb
+++ resources/dfile.rb
@@ -3,6 +3,8 @@ actions :create, :delete
 default_action :create
 
 attribute :block, :kind_of => [String, Array, NilClass], :default => nil # Include additional code
+attribute :reload, :kind_of => [Symbol], :equal_to => [:delayed, :immediately], :default => :delayed # How soon should we restart nginx.
+attribute :conf_name, :kind_of => [String, NilClass], :default => nil # Configuration name.
 
 def initialize(*args)
   super
